d1bb28efad20a346c0a76adf602ccfbd0603d507
Abhilash <abhilashbyaireddy@gmail.com>
2026-03-31 21:45:44 +0530

    counting words in a sentence

---
+++ Practices/countwords.py
@@ -0,0 +1,9 @@
+sentence = input("Enter a sentence:")
+
+# Split the sentence into a list of words based on whitespace
+words = sentence.split()
+
+# Get the count of elements in that list
+word_count = len(words)
+
+print(f"Word count: {word_count}")
