04dc392cc4cb9e5a57738af4eb821a801fe4313b
Abhilash <abhilashbyaireddy@gmail.com>
2026-04-10 22:04:27 +0530

    Finding sum of  list elements

---
+++ Practices/sum-list.py
@@ -0,0 +1,13 @@
+# Initializing the list
+numbers = [10, 20, 30, 40, 50]
+
+# Initializing total to 0
+total = 0
+
+# Iterating through each element in the list
+for num in numbers:
+    # Adding the current element to total
+    total += num
+
+# Displaying the result
+print("Sum of all elements:", total)
